289600f1b638419ede14695c651f48457863ccba
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2022-01-20 21:58:26 +0100

    Init

---
+++ src/main/kotlin/me/victorenaud/cuzcohotel/CuzcoHotelApplication.kt
@@ -0,0 +1,11 @@
+package me.victorenaud.cuzcohotel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CuzcoHotelApplication
+
+fun main(args: Array<String>) {
+	runApplication<CuzcoHotelApplication>(*args)
+}
---
+++ src/test/kotlin/me/victorenaud/cuzcohotel/CuzcoHotelApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.victorenaud.cuzcohotel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CuzcoHotelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
